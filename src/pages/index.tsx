c557bff39d1e09522b2d3168cd92dc52e01583fd
Ester Gama <esterfaniasantos@gmail.com>
2021-04-17 19:52:09 -0300

    feat: add input component on login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
+import { Flex, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
 export default function Home() {
-  return <h1>Dashgo</h1>;
+  return (
+    <Flex w='100vw' h='100vh' align='center' justify='center'>
+      <Flex
+        as='form'
+        width='100%'
+        maxWidth={360}
+        bg='gray.800'
+        p='8'
+        borderRadius={8}
+        flexDir='column'
+      >
+        <Stack spacing='4'>
+          <Input name='email' label='E-mail' type='email' />
+          <Input name='password' label='Password' type='password' />
+        </Stack>
+        <Button type='submit' mt='6' colorScheme='pink' size='lg'>
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
