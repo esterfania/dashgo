ad2573c3001b348e05dd2fd81b6ab08483b4e927
Ester Gama <esterfaniasantos@gmail.com>
2021-04-22 20:35:36 -0300

    styles: fix create user page

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -18,19 +18,19 @@ export default function CreateUser() {
   return (
     <Box>
       <Header />
-      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
+      <Flex w='100%' my={[4, 6]} maxWidth={1480} mx='auto' px={[4, 6]}>
         <Sidebar />
-        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+        <Box flex='1' borderRadius={8} bg='gray.800' p={[4, 6]}>
           <Heading size='lg' fontWeight='normal'>
             Criar usuário
           </Heading>
-          <Divider my='6' borderColor='gray.700' />
-          <VStack spacing='8'>
-            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+          <Divider my={[2, 6]} borderColor='gray.700' />
+          <VStack spacing={[2, 6]}>
+            <SimpleGrid minChildWidth='240px' spacing={[2, 6]} w='100%'>
               <Input name='name' label='Nome completo' />
               <Input name='email' type='email' label='E-mail' />
             </SimpleGrid>
-            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+            <SimpleGrid minChildWidth='240px' spacing={[2, 6]} w='100%'>
               <Input name='password' type='password' label='Senha' />
               <Input
                 name='password_confirmation'
@@ -39,7 +39,7 @@ export default function CreateUser() {
               />
             </SimpleGrid>
           </VStack>
-          <Flex mt='8' justify='flex-end'>
+          <Flex mt={[4, 6]} justify='flex-end'>
             <HStack spacing='4'>
               <Link href='/users'>
                 <Button colorScheme='whiteAlpha'>Cancelar</Button>
