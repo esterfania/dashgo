f1c3ac434c364085b45b3d65b092c920469b4fa9
esterfania <esterfaniasantos@gmail.com>
2021-05-26 22:47:00 -0300

    feat: rename comment component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,26 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { SidebarProvider } from '../components/contexts/SidebarContext';
 import { makeServer } from '../services/mirage';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarProvider>
-        <Component {...pageProps} />
-      </SidebarProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarProvider>
+          <Component {...pageProps} />
+        </SidebarProvider>
+      </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,6 +4,7 @@ import {
   Heading,
   Button,
   Icon,
+  Spinner,
   Table,
   Thead,
   Tr,
@@ -17,20 +18,38 @@ import {
 import Link from 'next/link';
 import React, { useEffect } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery(
+    'users',
+    async () => {
+      const response = await fetch('http://localhost:3000/api/users');
+      const data = await response.json();
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+          }),
+        };
+      });
+      return users;
+    },
+    {
+      staleTime: 1000 * 5,
+    }
+  );
   const isMobileVersion = useBreakpointValue({
     base: true,
     lg: false,
-  });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then((res) => res.json())
-      .then((res) => console.log(res.data));
   });
 
   return (
@@ -62,71 +81,62 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px='6' color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink'></Checkbox>
-                </Th>
-                <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th w='8'></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr px='6'>
-                <Td>
-                  <Checkbox colorScheme='pink'></Checkbox>
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Ester Gama</Text>
-                    <Text fontSize='small' color='gray.300'>
-                      esterfaniasantos@gmai.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>18 de Abril, 2021</Td>
-                <Td>
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='small'
-                    colorScheme='green'
-                    leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr px='6'>
-                <Td>
-                  <Checkbox colorScheme='pink'></Checkbox>
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Ester Gama</Text>
-                    <Text fontSize='small' color='gray.300'>
-                      esterfaniasantos@gmai.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>18 de Abril, 2021</Td>
-                <Td>
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='small'
-                    colorScheme='green'
-                    leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify='center'>
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Falha ao obter dados</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme='whiteAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th px='6' color='gray.300' width='8'>
+                      <Checkbox colorScheme='pink'></Checkbox>
+                    </Th>
+                    <Th>Usuário</Th>
+                    <Th>Data de cadastro</Th>
+                    <Th w='8'></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map((user) => {
+                    return (
+                      <Tr px='6' key={user.id}>
+                        <Td>
+                          <Checkbox colorScheme='pink'></Checkbox>
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight='bold'>{user.name}</Text>
+                            <Text fontSize='small' color='gray.300'>
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        <Td>{user.createdAt}</Td>
+                        <Td>
+                          <Button
+                            as='a'
+                            size='sm'
+                            fontSize='small'
+                            colorScheme='green'
+                            leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                          >
+                            Editar
+                          </Button>
+                        </Td>
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
