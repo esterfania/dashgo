08d03dbd3fd570f4e0ced82fdb4e9ce2065f242d
esterfania <esterfaniasantos@gmail.com>
2021-07-20 19:00:24 -0300

    fix date on users list

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,5 +1,5 @@
-import { useQuery } from 'react-query';
-import { api } from '../api';
+import { useQuery } from "react-query";
+import { api } from "../api";
 
 type User = {
   id: string;
@@ -14,22 +14,22 @@ type GetUsersResponse = {
 };
 
 export async function getUsers(page: number): Promise<GetUsersResponse> {
-  const { data, headers } = await api.get('users', {
+  const { data, headers } = await api.get("users", {
     params: {
       page,
     },
   });
 
-  const totalCount = Number(headers['x-total-count']);
+  const totalCount = Number(headers["x-total-count"]);
   const users = data.users.map((user) => {
     return {
       id: user.id,
       name: user.name,
       email: user.email,
-      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric',
+      createdAt: new Date(user.created_at).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
       }),
     };
   });
@@ -39,7 +39,7 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
   };
 }
 export function useUsers(page: number) {
-  return useQuery(['users', page], () => getUsers(page), {
+  return useQuery(["users", page], () => getUsers(page), {
     staleTime: 1000 * 5,
   });
 }
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -4,8 +4,8 @@ import {
   Model,
   Response,
   ActiveModelSerializer,
-} from 'miragejs';
-import faker from 'faker';
+} from "miragejs";
+import faker from "faker";
 type User = {
   name: string;
   email: string;
@@ -33,35 +33,35 @@ export function makeServer() {
       }),
     },
     seeds(server) {
-      server.createList('user', 10);
+      server.createList("user", 30);
     },
     routes() {
-      this.namespace = 'api';
+      this.namespace = "api";
       this.timing = 750;
 
-      this.get('/users', function (schema, request) {
+      this.get("/users", function (schema, request) {
         const { page = 1, per_page = 10 } = request.queryParams;
-        const total = schema.all('user').length;
+        const total = schema.all("user").length;
         const pageStart = (Number(page) - 1) * Number(per_page);
         const pageEnd = pageStart + Number(per_page);
 
-        const users = this.serialize(schema.all('user')).users.slice(
+        const users = this.serialize(schema.all("user")).users.slice(
           pageStart,
           pageEnd
         );
 
         return new Response(
           200,
-          { 'x-total-count': String(total) },
+          { "x-total-count": String(total) },
           {
             users,
           }
         );
       });
-      this.get('/users/:id');
-      this.post('/users');
+      this.get("/users/:id");
+      this.post("/users");
 
-      this.namespace = '';
+      this.namespace = "";
       this.passthrough();
     },
   });
